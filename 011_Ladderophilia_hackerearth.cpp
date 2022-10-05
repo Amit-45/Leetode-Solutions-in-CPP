52cdfcf43f4cf5751c77cc2512f504ddf8ce5cb5
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-05 16:44:39 +0530

    Create 011_Ladderophilia_hackerearth.cpp

---
+++ 011_Ladderophilia_hackerearth.cpp
@@ -0,0 +1,23 @@
+https://www.hackerearth.com/problem/algorithm/ladderophilia-2024b9c9/
+
+// We have populated the solutions for the 10 easiest problems for your support.
+// Click on the SUBMIT button to make a submission to this problem.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n ;
+  cin>>n;
+
+  cout << "*   *" << endl;
+  cout << "*   *" << endl;
+
+  for (int i = 1; i <= n; i++)
+  {
+
+    cout << "*****" << endl;
+    cout << "*   *" << endl;
+    cout << "*   *" << endl;
+  }
+}
