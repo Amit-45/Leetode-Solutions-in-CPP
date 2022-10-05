b5ab88407ded810d8f1a5821cc4ab4514d05e692
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-05 16:34:53 +0530

    Create 006_Life, the Universe, and Everything_Spoj.cpp

---
+++ 006_Life, the Universe, and Everything_Spoj.cpp
@@ -0,0 +1,20 @@
+Link-https://www.spoj.com/problems/TEST/
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	
+	 while(1)
+    {
+        int n;
+        cin>>n;
+        if(n==42)
+        {
+            break;
+        }
+        cout<<n<<endl;
+    }
+ 
+	return 0;
+} 
