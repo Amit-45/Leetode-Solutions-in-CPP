f04b763bab2311a52f5a7e8121eab2425d7f9242
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-05 16:42:27 +0530

    Create 010_Mangoes_hackerearth.cpp

---
+++ 010_Mangoes_hackerearth.cpp
@@ -0,0 +1,17 @@
+Link-https://www.hackerearth.com/problem/algorithm/mangoes/description/
+#include <iostream>
+using namespace std;
+int main() {
+	int num;
+	cin >> num;    //Reading input from STDIN
+	
+	if(num%2==0 && num>2)
+	
+	{
+		cout<<"YES"<<endl;
+	}
+	else
+	{
+		cout<<"NO"<<endl;
+	}
+}
