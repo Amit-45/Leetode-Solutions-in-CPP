a75a72ff10e7b1d7d488f9f243aeda95f4b35242
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-07 03:46:50 +0530

    Update 023_Union Of Two Sorted Arrays.cpp

--- 023_Union Of Two Sorted Arrays.cpp
+++ 023_Union Of Two Sorted Arrays.cpp
@@ -57,6 +57,69 @@ int main()
     cout << pair << endl;
   }
 }
+//using set
+class Solution
+{
+public:
+  vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+  {
+
+    set<int> s;
+    vector<int> Union;
+    for (int i = 0; i < n; i++)
+      s.insert(arr1[i]);
+    for (int i = 0; i < m; i++)
+      s.insert(arr2[i]);
+    for (auto &it : s)
+      Union.push_back(it);
+    return Union;
+  }
+};
+
+//Set approach(explained)
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+  // array elements
+  int arr1[] = {1, 2, 3, 4, 5};
+  int n = 5;
+  int arr2[] = {2, 3, 4, 4, 5, 10};
+  int m = 6;
+
+  // set to store  1st array elements
+  set<int> s;
+  for (int i = 0; i < n; i++)
+    s.insert(arr1[i]);
+  for (auto &pair : s)
+  {
+    cout << pair << endl;
+  }
+  cout << endl;
+
+  // set to store 2nd array elements (duplicates will not be stored)
+  for (int i = 0; i < m; i++)
+    s.insert(arr2[i]);
+  for (auto &pair : s)
+  {
+    cout << pair << endl;
+  }
+  cout << endl;
+
+  // pushing all key values to union vector
+  vector<int> Union;
+  for (auto &it : s)
+    Union.push_back(it);
+
+  // printing out the Union vector's elements
+  for (auto &pair2 : Union)
+  {
+    cout << pair2 << endl;
+  }
+}
+
+
 
 
 
