380bc0302aeca44eeca15d609ff983a768d4a93c
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-11 18:54:51 +0530

    Update and rename 040_Subarray with sum 0.cpp to 040_Length of largest subarray with sum 0.cpp

--- 040_Length of largest subarray with sum 0.cpp
+++ 040_Length of largest subarray with sum 0.cpp
@@ -1,4 +1,25 @@
 ===========================BRUTE FORCE=====================================================================================================
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+    int  maxi = 0;
+	for(int i = 0; i < n; ++i){
+		int sum = 0;
+		for(int j = i; j < n; ++j){
+			sum += A[j];
+			if(sum == 0){
+				maxi = max(maxi, j-i+1);
+			}
+		}
+	}
+	return maxi;
+    }
+};
+
+  
+  
+====================== CHECK IF SUBARRAY OF SUM 0 EXISTS=====================================================================================
 #include <bits/stdc++.h>
 using namespace std;
 int main()
