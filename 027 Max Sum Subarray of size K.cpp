80498c0ce8a94e56504b0edef801a5195383f823
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-11 19:06:34 +0530

    Update 027 Max Sum Subarray of size K.cpp

--- 027 Max Sum Subarray of size K.cpp
+++ 027 Max Sum Subarray of size K.cpp
@@ -1,3 +1,42 @@
+================================BRUTE FORCE=================================================================================TLE==================
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestSubArrWithSumK_BF(int arr[], int n, int k) {
+  int maxLength = 0;
+  // finding subarrays using i and j loop 
+  for (int i = 0; i < n; i++) {
+    int sum = 0;
+    for (int j = i; j < n; j++) {
+      //Also finding the sum of all the sub arrays suring the sme iteration 
+      sum += arr[j];
+      //When you reach  your desired sum
+      if (sum == k)
+        //Record the lengths of those sub arrays and keep comparing with maxLength 
+        //Update max if you find any subaray length greater than the maxLength
+        //(j-i+1 gives the length of the subarray)
+        maxLength = max(maxLength, (j - i + 1));
+    }
+
+  }
+  return maxLength;
+}
+
+int main() {
+
+  int arr[] = {7,1,6,0};
+  int n = sizeof(arr) / sizeof(arr[0]), k = 7;
+
+  cout << "Length of the longest subarray with sum K is " << 
+  longestSubArrWithSumK_BF(arr, n, k);
+
+  return 0;
+}
+
+==================================================================================SLIDING WINDOW APPROACH=================================================
+  (ONLY WORKS FOR POSITIVE INPUTS)
+  ----------------------------------
 public:
 long maximumSumSubarray(int K, vector<int> &Arr, int N)
 {
