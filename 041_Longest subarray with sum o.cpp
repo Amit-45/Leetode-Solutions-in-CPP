0cf9a1276aae63b4088130a65cf239b2d46a2b86
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-11 22:26:15 +0530

    Update 041_Longest subarray with sum o.cpp

--- 041_Longest subarray with sum o.cpp
+++ 041_Longest subarray with sum o.cpp
@@ -1,5 +1,7 @@
  // Your code here
 https://github.com/Amit-45/Leetode-Solutions-in-C-/blob/main/028_Longest%20subarray%20having%20sum%20k.cpp
+
+Striver video:   https://www.youtube.com/watch?v=xmguZ6GbatA&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=25
     unordered_map<int,int> mpp; 
     int maxi = 0;
     int sum = 0; 
