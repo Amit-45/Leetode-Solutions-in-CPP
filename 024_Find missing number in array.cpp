289f9521055a4d0bb76fec1398fac01a9ace5d25
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-11 17:56:57 +0530

    Update 024_Find missing number in array.cpp

--- 024_Find missing number in array.cpp
+++ 024_Find missing number in array.cpp
@@ -55,4 +55,41 @@ pair<int, int> missingAndRepeating(vector<int> &arr, int n)
 
   return {missingNumber, repeating};
 }
+
+
+//Optimised approach (Sum and sum square approach) GFG(https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=find-missing-and-repeating)
+//have to return in int * 
+
+typedef long long int ll;
+class Solution{
+public:
+    int *findTwoElement(int *arr, int n)
+    
+    {
+     
+     ll len = n;
+
+  ll  S= (len * (len + 1)) / 2;
+  ll  P = (len * (len + 1) * (2 * len + 1)) / 6;
+  ll  missingNumber = 0, repeating = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    S -= (ll )arr[i];
+    P -= (ll )arr[i] * (ll )arr[i];
+  }
+
+  missingNumber = (S + P / S) / 2;
+
+  repeating = missingNumber - S;
+
+  
+    
+    int *ans=new int(2);
+    ans[0]=repeating;
+    ans[1]=missingNumber;
+       
+     return ans ;   
+    }
+};
 //Optimised approach (XOR approach)
