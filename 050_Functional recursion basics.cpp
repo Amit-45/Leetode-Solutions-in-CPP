a2390d8192ebee96d17f07c2e4019194327e97bc
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-17 19:42:19 +0530

    Update 050_Functional recursion basics.cpp

--- 050_Functional recursion basics.cpp
+++ 050_Functional recursion basics.cpp
@@ -1,4 +1,4 @@
-----------------Sum of N natural numbers(using recursion)--------------------------------------------------------------------------------------------
+----------------Sum of N natural numbers(using functional recursion)--------------------------------------------------------------------------------------------
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,7 +12,7 @@ public:
     return n + (sumOfSeriesTillN(n - 1));
   }
 };
-
+--------------------------------Fibonacci( using functional recursion)--------------------------------------------------------------------------------------------
 int main()
 {
   int T;
@@ -24,6 +24,36 @@ int main()
     cin >> N;
     Solution ob;
     cout << ob.sumOfSeriesTillN(N);
+    cout << "\n";
+  }
+  return 0;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int fibonacciUsingRecursion(int n)
+  {
+    if (n == 0)
+      return 1;
+    return n * (fibonacciUsingRecursion(n - 1));
+  }
+};
+
+int main()
+{
+  int T;
+  cin >> T;
+
+  while (T--)
+  {
+    long long N;
+    cin >> N;
+    Solution ob;
+    cout << ob.fibonacciUsingRecursion(N);
     cout << "\n";
   }
   return 0;
