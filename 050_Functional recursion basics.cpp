033cee908cb7fa0ae39099cec5099c968b985693
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-17 22:07:49 +0530

    Update 050_Functional recursion basics.cpp

--- 050_Functional recursion basics.cpp
+++ 050_Functional recursion basics.cpp
@@ -93,7 +93,7 @@ int main()
   }
   return 0;
 }
----------------------Reverse array using recursion---------------------------------------------------------------------------------
+---------------------Reverse array using recursion(using two pointees)---------------------------------------------------------------------------------
   #include <bits/stdc++.h>
 using namespace std;
 
@@ -122,5 +122,38 @@ int main()
     cin >> arr[i];
   }
   reverseArray(arr, 0, n - 1);
+  printArray(arr, n);
+}
+
+---------------------Reverse array using recursion(using one pointer)---------------------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int arr[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+}
+void reverseArray(int arr[], int i, int n)
+{
+  if (i >= n / 2)
+    return;
+
+  swap(arr[i], arr[n - i - 1]);
+  reverseArray(arr, i + 1, n);
+}
+int main()
+{
+  int n, i;
+  cin >> n;
+  int arr[n];
+  for (i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  reverseArray(arr, 0, n);
   printArray(arr, n);
 }
