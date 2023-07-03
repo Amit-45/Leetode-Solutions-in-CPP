70dfbcc4911650c77fdfd59fbc95eb86da4c8c99
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2023-07-03 19:38:47 +0530

    Time: 53 ms (61.81%), Space: 18.9 MB (79.45%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1;
-        int right = getMaxPile(piles);
+        int left = 1; // Starting range for eating speed
+        int right = getMaxPile(piles); // Maximum pile size
+        int answer = -1; // Variable to store the result
         
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+        while (left <= right) { // Use <= instead of < in the loop condition
+            int mid = left + (right - left) / 2; // Calculate the middle speed
             
             if (canEatWithinTime(piles, mid, h)) {
-                right = mid;
+                // If eating within time is possible, adjust the right boundary and update the answer
+                right = mid - 1;
+                answer = mid;
             } else {
+                // If eating within time is not possible, adjust the left boundary
                 left = mid + 1;
             }
         }
         
-        return left;
+        return answer; // Return the minimum eating speed
     }
     
 private:
     int getMaxPile(vector<int>& piles) {
         int maxPile = INT_MIN;
         
+        // Find the maximum pile size
         for (int pile : piles) {
             maxPile = max(maxPile, pile);
         }
@@ -29,16 +34,17 @@ private:
     }
     
     bool canEatWithinTime(vector<int>& piles, int speed, int h) {
-        long long totalHours = 0;
+        long long totalHours = 0; // Total hours taken to eat all piles
         
         for (int pile : piles) {
             totalHours += ceil((double)pile / (double)speed);
             
             if (totalHours > h) {
+                // If total hours exceed the given limit, eating is not possible within time
                 return false;
             }
         }
         
-        return true;
+        return true; // Eating is possible within time
     }
 };
