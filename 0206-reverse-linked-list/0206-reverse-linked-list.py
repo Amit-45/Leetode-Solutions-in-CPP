667dad518d4bba4f604189e6a048e9298625a10e
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2023-10-03 03:32:21 +0530

    Time: 16 ms (73.5%), Space: 18.6 MB (9.15%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -1,26 +1,36 @@
-#Just as in diagram : take two pointers curr(pointing to 1st node) and prev (pointing to NULL)
-# We want to keep going till we reach the end of the list using condition: while curr is not null
-#Then we reverse the pointers : by curr.next = prev
-#Then we can shift our pointers : prev=curr, curr=curr.next
-#We can store this curr.next in 1st line in a variable called "nxt" so that we can use it later (in place of curr.next while shifting of pointers)
-#Lastly we return the result ie. prev because at the last step of our approach 
+#Given: head->1->2->3->4
+#After recursion part reversed the part from 2 to 4 and returned a new head : ->1->[2<-3<-4]<-head 
+
+#---------------reverse function--------------------------------
+#Create a function named reverse that would take params (self,head)
+#Create a "reverse_head" variable that will return the new head after reversing using recursion
+#Recursion call would be something like: reverse_head = self.reverse(head.next) which was as mentioned was going to give a new head
+#Then my work :-
+#   =>Link 2 and 1  (head.next.next = head)(i am telling 2 to point to head node ie 1)
+#   =>Link 1 and None (head.next = None) (i am telling 1 to point to None)
+#Finally return the reverse Head
+
+#-----------Base case for reverse function-----------------
+# -If the list has only one node: it returns the same node because reversing a single-node list results in the same list (Checked with condition :if not head.next)
+# -If the list has no node or Empty node:(Checked with condition :if not head)
+
+#---------reverseList function------------------------------
+#It will simply return the head after the reverse
+
 
 class Solution(object):
+    def reverse(self, head):
+        # Base case for recursion
+        if not head or not head.next:
+            return head
+        
+        reverse_head = self.reverse(head.next) #recursive call
+        head.next.next = head 
+        head.next = None
+        return reverse_head
+
     def reverseList(self, head):
-        prev, curr = None, head
-        while curr: 
-            temp=curr.next #added at last
-            curr.next=prev
-            prev=curr
-            curr=temp
-        return prev
-
-
-
-
-
-
-
+        return self.reverse(head)
 
 
 # Definition for singly-linked list.
