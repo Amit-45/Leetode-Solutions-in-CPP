3dfbc53f2c25310a81bf99726f321c258e545c64
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2023-10-08 09:20:36 +0530

    Time: 15 ms (98.66%), Space: 13.7 MB (25.44%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -34,30 +34,36 @@
 #             if matrix[i][0] <= target <= matrix[i][m - 1]:
 #                 return self.binarySearch(matrix[i], target)
 #         return False
-#Flattening an array approach:
-class Solution(object):
-    def binarySearch(self, nums, target,m):
-        n = len(nums)
-        low, high = 0, n - 1
 
+
+#Flattening the array approach:
+# Place the 2 pointers i.e. low and high: Initially, we will place the pointers. The pointer low will point to 0 and the high will point to (NxM)-1. : 
+# Calculate the ‘mid’: Now, inside the loop, we will calculate the value of ‘mid’ using the following formula:
+# mid = (low+high) // 2 ( ‘//’ refers to integer division)
+# Eliminate the halves based on the element at index mid: To get the element, we will convert index ‘mid’ to the corresponding cell using the above formula. Here no. of columns of the matrix = M.
+# row = mid / M, col = mid % M.
+# If matrix[row][col] == target: We should return true here, as we have found the ‘target’.
+# If matrix[row][col] < target: In this case, we need bigger elements. So, we will eliminate the left half and consider the right half (low = mid+1).
+# If matrix[row][col] > target: In this case, we need smaller elements. So, we will eliminate the right half and consider the left half (high = mid-1).
+# Steps 2-3 will be inside a while loop and the loop will end once low crosses high
+# (i.e. low > high). If we are out of the loop, we can say the target does not exist in the matrix. So, we will return false.
+class Solution(object):
+    def searchMatrix(self,matrix, target):
+        n = len(matrix)
+        m = len(matrix[0])
+
+        # apply binary search:
+        low = 0
+        high = n * m - 1
         while low <= high:
             mid = (low + high) // 2
             row = mid // m
             col = mid % m
-            if nums[mid] == target:
+            if matrix[row][col] == target:
                 return True
-            elif nums[mid]< target:
+            elif matrix[row][col] < target:
                 low = mid + 1
             else:
                 high = mid - 1
-        return False
-
-    def searchMatrix(self, matrix, target):
-        n = len(matrix)
-        m = len(matrix[0])
-
-        for i in range(n):
-            if matrix[i][0] <= target <= matrix[i][m - 1]:
-                return self.binarySearch(matrix[i], target,m)
         return False
 
