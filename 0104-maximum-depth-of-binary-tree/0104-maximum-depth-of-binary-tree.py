f3d1cc56df5f805b369437bae941b26c6965437a
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2023-10-09 16:50:43 +0530

    Time: 34 ms (5.32%), Space: 15.9 MB (57.71%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -29,28 +29,61 @@
 
 
 
+# class Solution:
+#     def maxDepth(self, root):
+#         if not root:
+#             return 0
+        
+#         level = 0
+#         q = deque([root])
+        
+#         while q:
+            
+#             level_size = len(q)  
+            
+#             for i in range(level_size):
+#                 node = q.popleft()
+                
+#                 if node.left:
+#                     q.append(node.left)
+                
+#                 if node.right:
+#                     q.append(node.right)
+#             level += 1 
+#         return level
+
+
+# Iterative DFS/ pre order traversal for finding max depth of binary tree------------------------------------
+#Base case : Tree empty return 0 bacause the max depth of a empty tree is 0
+#Take a stack with initial value of root but remember we also have a depth too .So we take a pair of values.
+#stack = [[root, 1]]
+#We will do the DFS till the stack is empty : while stack:
+#Now we pop from stack 
+#We pop 2 values . Remember we have to pop up the pair.So to pop up the pair we can use : node, depth = stack.pop()
+#It is possible that the node can be Null do use this condition :   if node:
+#If it is not null then, we update our result .For that first let the result be 1 because we have a default depth of 1 : res = 1 
+#Then we update our max depth or result by max function : res = max(res, depth)
+#Then to our stack we add the chidren of the code we are currently at 
+#For that I use the stack.append([node.left,node.right ])
+#Acually we have to also append the depth also for each of the children .We can do that to both l and r by  : 
+#stack.append([node.left, depth + 1]) and stack.append([node.right, depth + 1])
+#At last when stack becomes empty return the result 
 class Solution:
-    def maxDepth(self, root):
+    def maxDepth(self, root) :
         if not root:
             return 0
-        
-        level = 0
-        q = deque([root])
-        
-        while q:
-            
-            level_size = len(q)  
-            
-            for i in range(level_size):
-                node = q.popleft()
-                
-                if node.left:
-                    q.append(node.left)
-                
-                if node.right:
-                    q.append(node.right)
-            level += 1 
-        return level
+        stack = [[root, 1]]
+        res = 1
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+
+        return res
 
 
 # Definition for a binary tree node.
