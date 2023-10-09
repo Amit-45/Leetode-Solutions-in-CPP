09580f8acab4f7aeb2ec5bb9b8b05409c6cf456c
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2023-10-09 16:51:14 +0530

    Time: 31 ms (5.32%), Space: 15.9 MB (57.71%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -2,11 +2,11 @@
 #Base case : Tree empty return 0 bacause the max depth of a empty tree is o
 #If 1 node and no childern: max of left and right subtree = 0, So return 1+max(left),max(right) = 1
 #If 1 node with children : Use resursive dfs for left and right sub tree) 
-# class Solution:
-#     def maxDepth(self, root):
-#         if not root:
-#             return 0
-#         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 #Iterative BFS/ level order traversal for finding max depth of binary tree----------------------------------------
 #Base case : Tree empty return 0 bacause the max depth of a empty tree is 0
@@ -29,28 +29,28 @@
 
 
 
-# class Solution:
-#     def maxDepth(self, root):
-#         if not root:
-#             return 0
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
         
-#         level = 0
-#         q = deque([root])
+        level = 0
+        q = deque([root])
         
-#         while q:
+        while q:
             
-#             level_size = len(q)  
+            level_size = len(q)  
             
-#             for i in range(level_size):
-#                 node = q.popleft()
+            for i in range(level_size):
+                node = q.popleft()
                 
-#                 if node.left:
-#                     q.append(node.left)
+                if node.left:
+                    q.append(node.left)
                 
-#                 if node.right:
-#                     q.append(node.right)
-#             level += 1 
-#         return level
+                if node.right:
+                    q.append(node.right)
+            level += 1 
+        return level
 
 
 # Iterative DFS/ pre order traversal for finding max depth of binary tree------------------------------------
