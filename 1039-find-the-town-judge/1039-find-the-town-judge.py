096bf4f431adfe57bf75f981878a156ec254d7a3
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2023-10-28 23:06:31 +0530

    Time: 556 ms (35.02%), Space: 18.9 MB (6.06%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findJudge(self, n, trust):
+        d = {}
+        for i in trust:
+            try:
+                x = d[i[0]]
+                x.append(i[1])
+                d[i[0]] = x
+            except KeyError:
+                d[i[0]] = [i[1]]
+        
+        townjudgeCandidates = []
+        for i in range(1,n+1):
+            try:
+                x = d[i]
+            except KeyError:
+                d[i] = []
+                townjudgeCandidates.append(i)
+        
+        if len(townjudgeCandidates) == 0:
+            return -1
+        
+        found = True
+        for cand in townjudgeCandidates:
+            for i in range(1,n+1):
+                if i != cand:
+                    if cand not in d[i]:
+                        found = False
+                        break
+            if found == True:
+                return cand
+        if found == False:
+            return -1
+        
